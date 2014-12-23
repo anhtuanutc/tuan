f67bf2904511dafccc4f2b2cb8ca29bd2aa4ff98
Masaaki Morishita <ikasama3.0@gmail.com>
2014-12-23 23:26:22 +0900

    initialize init(), add(), list() methods

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-module.exports = function () {
+var fs = require('fs');
+var exec = require('child_process').exec;
+var mkdirp = require('mkdirp');
+var moment = require('moment');
+
+
+module.exports = Tuan;
+
+
+function Tuan () {
+    if (!(this instance of Tuan)) {
+        return new Tuan();
+    }
 }
+
+
+
+Tuan.prototype.init = function () {
+    mkdirp('.tuan', function (err) {
+        if (err) {
+            throw err;
+        }
+    })
+};
+
+
+Tuan.prototype.add = function (message, command) {
+    exec(command, function (err, stdout, stderr) {
+        if (!err) {
+            console.log(stdout);
+
+            var dt = moment();
+            var fileName = command.split(' ')[1];
+            var fileContents = fs.readFileSync(fileName, 'utf-8');
+
+            fs.writeFile('./tuan/' + message + '__' + dt.format('YYYY-MM-DD HH:mm:ss'), fileContents, function (err) {
+                if (err) {
+                    throw err;
+                }
+            });
+        } else {
+            throw err;
+        }
+    })
+};
+
+
+Tuan.prototype.list = function () {
+    exec('ls ./tuan', function (err, stdout, stderr) {
+        if (!err) {
+            console.log(stdout);
+        } else {
+            throw err;
+        }
+    })
+};
